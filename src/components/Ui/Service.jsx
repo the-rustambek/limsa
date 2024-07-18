5ed67dc28cc105153d60800a029948c7327aeecb
sunnatbekweb <sunnatbekkadamov@gmail.com>
2024-07-18 15:17:06 +0500

    fixed icons

--- src/components/Ui/Service.jsx
+++ src/components/Ui/Service.jsx
@@ -10,7 +10,7 @@ import { useTranslation } from "react-i18next";
 import Link from "next/link";
 
 const Service = () => {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div className={styles.service_container}>
@@ -27,24 +27,32 @@ const Service = () => {
           <h3 className={styles.service_card_title}>{t("websites")}</h3>
           <ul className={styles.service_text_list}>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              </div>
               <p className={styles.service_card_text}>{t("webText1")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              </div>
               <p className={styles.service_card_text}>{t("webText2")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              </div>
               <p className={styles.service_card_text}>{t("webText3")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px", width: 20, height: 20 }} />
+              </div>
               <p className={styles.service_card_text}>{t("webText4")}</p>
             </li>
           </ul>
           <Link href="#contact">
-             <button className={styles.contact_button}>{t("call")}</button>
+            <button className={styles.contact_button}>{t("call")}</button>
           </Link>
         </div>
       </div>
@@ -61,17 +69,22 @@ const Service = () => {
           <h3 className={styles.service_card_title}>{t("mobile")}</h3>
           <ul className={styles.service_text_list}>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>Android</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>IOS</p>
             </li>
           </ul>
           <Link href="#contact">
-             <button className={styles.contact_button}>{t("call")}</button>
-          </Link>        </div>
+            <button className={styles.contact_button}>{t("call")}</button>
+          </Link>{" "}
+        </div>
       </div>
       <div className={styles.service_card}>
         <div className={styles.image_container}>
@@ -86,25 +99,34 @@ const Service = () => {
           <h3 className={styles.service_card_title}>{t("telegram_bots")}</h3>
           <ul className={styles.service_text_list}>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("botText1")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("botText2")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("botText3")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("botText4")}</p>
             </li>
           </ul>
           <Link href="#contact">
-             <button className={styles.contact_button}>{t("call")}</button>
-          </Link>        </div>
+            <button className={styles.contact_button}>{t("call")}</button>
+          </Link>{" "}
+        </div>
       </div>
       <div className={styles.service_card}>
         <div className={styles.image_container}>
@@ -119,25 +141,34 @@ const Service = () => {
           <h3 className={styles.service_card_title}>{t("graphicDesign")}</h3>
           <ul className={styles.service_text_list}>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("graphicDesText1")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("graphicDesText2")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("graphicDesText3")}</p>
             </li>
             <li className={styles.list_item}>
-              <FaCheck style={{ fontSize: "20px" }} />
+              <div>
+                <FaCheck style={{ fontSize: "20px" }} />
+              </div>
               <p className={styles.service_card_text}>{t("graphicDesText4")}</p>
             </li>
           </ul>
           <Link href="#contact">
-             <button className={styles.contact_button}>{t("call")}</button>
-          </Link>        </div>
+            <button className={styles.contact_button}>{t("call")}</button>
+          </Link>{" "}
+        </div>
       </div>
     </div>
   );
